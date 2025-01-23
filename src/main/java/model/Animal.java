d7f5c733338d28b73a2114917bd0fa2a0f89cd75
jose30-back <josema.bootcamp@gmail.com>
2025-01-23 10:10:36 +0100

    model and configurations

--- src/main/java/model/Animal.java
+++ src/main/java/model/Animal.java
@@ -1,5 +1,90 @@
-package models;
+package model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Animal {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String type;
+    private String family;
+    private String gender;
+    private String country;
+    private LocalDate dateOfEntry;
+
+    public Animal(long id, String name, String type, String family, String gender, String country,
+            LocalDate dateOfEntry) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.family = family;
+        this.gender = gender;
+        this.country = country;
+        this.dateOfEntry = dateOfEntry;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public LocalDate getDateOfEntry() {
+        return dateOfEntry;
+    }
+
+    public void setDateOfEntry(LocalDate dateOfEntry) {
+        this.dateOfEntry = dateOfEntry;
+
+    }
 }
